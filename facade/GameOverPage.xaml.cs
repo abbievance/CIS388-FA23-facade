9d3cd92ef60b8f2de03c03d9b051be6cb4b95d8b
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-10-06 15:55:17 -0500

    I know this is super horrible code but this is the only way I found to get previous guesses to work

--- facade/GameOverPage.xaml.cs
+++ facade/GameOverPage.xaml.cs
@@ -6,28 +6,60 @@ namespace facade;
 public partial class GameOverPage : ContentPage
 
 {
-    private bool didWin;
-    public bool DidWin
-    {
-        get => didWin;
-        set
-        {
-            didWin = value;
-            if (didWin)
-            {
-                ResultLabel.Text = "You Won!";
-            }
-            else
-            {
-                ResultLabel.Text = "You Lost!";
-            }
-        }
-    }
 
-
-    public GameOverPage()
+    public GameOverPage(bool didWin, ObservableCollection<ColorGuess> guesses)
 	{
 		InitializeComponent();
+
+        if (didWin)
+        {
+            ResultLabel.Text = "You Won!";
+        }
+        else
+        {
+            ResultLabel.Text = "You Lost!";
+        }
+
+        switch (guesses.Count)
+        {
+            case 1:
+                guess1.BackgroundColor = guesses[0].Color;
+                break;
+            case 2:
+                guess1.BackgroundColor = guesses[0].Color;
+                guess2.BackgroundColor = guesses[1].Color;
+                break;
+            case 3:
+                guess1.BackgroundColor = guesses[0].Color;
+                guess2.BackgroundColor = guesses[1].Color;
+                guess3.BackgroundColor = guesses[2].Color;
+                break;
+            case 4:
+                guess1.BackgroundColor = guesses[0].Color;
+                guess2.BackgroundColor = guesses[1].Color;
+                guess3.BackgroundColor = guesses[2].Color;
+                guess4.BackgroundColor = guesses[3].Color;
+                break;
+            case 5:
+                guess1.BackgroundColor = guesses[0].Color;
+                guess2.BackgroundColor = guesses[1].Color;
+                guess3.BackgroundColor = guesses[2].Color;
+                guess4.BackgroundColor = guesses[3].Color;
+                guess5.BackgroundColor = guesses[4].Color;
+                break;
+            case 6:
+                guess1.BackgroundColor = guesses[0].Color;
+                guess2.BackgroundColor = guesses[1].Color;
+                guess3.BackgroundColor = guesses[2].Color;
+                guess4.BackgroundColor = guesses[3].Color;
+                guess5.BackgroundColor = guesses[4].Color;
+                guess6.BackgroundColor = guesses[5].Color;
+                break;
+
+        }
+
+
+
     }
     async void Button_Clicked(object sender, EventArgs e)
     {
--- facade/MainPageViewModel.cs
+++ facade/MainPageViewModel.cs
@@ -60,13 +60,13 @@ namespace facade
             if (CurrentGuess.Equals(SecretColor))
             {
                 DidWin = true;
-                await Shell.Current.GoToAsync($"{nameof(GameOverPage)}?DidWin={DidWin}");
+                await Shell.Current.Navigation.PushAsync(new GameOverPage(DidWin, Guesses));
                 Guesses.Clear();
             }
             if (Guesses.Count == 6 && (CurrentGuess.Equals(SecretColor) == false))
             {
                 DidWin = false;
-                await Shell.Current.GoToAsync($"{nameof(GameOverPage)}?DidWin={DidWin}");
+                await Shell.Current.Navigation.PushAsync(new GameOverPage(DidWin, Guesses));
                 Guesses.Clear();
             }
             CurrentGuess = "";
