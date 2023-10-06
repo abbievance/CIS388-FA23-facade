6c9702c3188d812e217d4f6add93ccc972c3c9f4
Abbie Vance <89212624+abbievance@users.noreply.github.com>
2023-10-06 14:58:14 -0500

    previous guesses works now

--- facade/GameOverPage.xaml.cs
+++ facade/GameOverPage.xaml.cs
@@ -6,28 +6,27 @@ namespace facade;
 public partial class GameOverPage : ContentPage
 
 {
-    private bool didWin;
-    public bool DidWin
-    {
-        get => didWin;
-        set
-        {
-            didWin = value;
-            if (didWin)
-            {
-                ResultLabel.Text = "You Won!";
-            }
-            else
-            {
-                ResultLabel.Text = "You Lost!";
-            }
-        }
-    }
 
 
-    public GameOverPage()
+    public GameOverPage(bool didWin, ObservableCollection<ColorGuess> guesses)
 	{
 		InitializeComponent();
+
+        if (didWin)
+        {
+            ResultLabel.Text = "You Won!";
+        }
+        else
+        {
+            ResultLabel.Text = "You Lost!";
+        }
+
+        guess1.BackgroundColor = guesses[0].Color;
+        guess2.BackgroundColor = guesses[1].Color;
+        guess3.BackgroundColor = guesses[2].Color;
+        guess4.BackgroundColor = guesses[3].Color;
+        guess5.BackgroundColor = guesses[4].Color;
+        guess6.BackgroundColor = guesses[5].Color;
     }
     async void Button_Clicked(object sender, EventArgs e)
     {
